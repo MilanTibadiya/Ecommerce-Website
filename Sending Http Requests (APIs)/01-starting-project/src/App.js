3752b4542d1d3ca0619b146781c4de6066218321
MilanTibadiya <tibadiyamilan@gmail.com>
2023-02-05 18:52:17 +0530

    09-(Fetch Movie on Page load)

--- Sending Http Requests (APIs)/01-starting-project/src/App.js
+++ Sending Http Requests (APIs)/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,12 +8,13 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const ref = useRef(true);
+  // console.log(ref)
 
-  async function fetchMoviesHandler() {  
+  const fetchMoviesHandler = useCallback( async () => {  
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/film/')
+      const response = await fetch('https://swapi.dev/api/films/')
     if(!response.ok){
       throw new Error('Something went wrong!  ')
     }
@@ -36,7 +37,11 @@ function App() {
       if(ref.current) setTimeout(() => fetchMoviesHandler(), 5000)
     }
     setIsLoading(false);
-  }
+  }, [] );
+
+  useEffect(() => {          
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   // let content = <p>Found no movies.</p>;
 
