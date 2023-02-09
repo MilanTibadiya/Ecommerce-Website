1cbba15c140a0b69cc5a864a200c4eb33e975918
MilanTibadiya <tibadiyamilan@gmail.com>
2023-02-09 16:18:58 +0530

    20 ( Addressing security and logout issues )

--- Signup and Authentication/authentication/src/store/auth-context.js
+++ Signup and Authentication/authentication/src/store/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null);
+    const initialToken = localStorage.getItem('ID'); //for when refresh page data set in settoken,dont login again
+    const [token, setToken] = useState(initialToken);
 
     const userIsLoggedIn = !!token; //!! is trick of js 2nd ! for val available or nor && 1st ! give boolean for that true/false
 
     const loginHandler = (token) => {
         setToken(token);
+        localStorage.setItem('ID', token); //inseted data always on string here we already have otherwise sringify data
     };
 
     const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('ID');
     };
 
     const contextValue = {
