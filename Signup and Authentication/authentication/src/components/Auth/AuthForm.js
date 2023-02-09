ce2cb8d46133371efb331064d976c9b30df0a5ef
MilanTibadiya <tibadiyamilan@gmail.com>
2023-02-09 11:53:36 +0530

    18 ( Updating the Old Password )

--- Signup and Authentication/authentication/src/components/Auth/AuthForm.js
+++ Signup and Authentication/authentication/src/components/Auth/AuthForm.js
@@ -1,9 +1,11 @@
 import { useState, useRef, useContext } from 'react';
-import AuthContext from '../../store/auth-context';
+import { useHistory } from 'react-router-dom';
 
+import AuthContext from '../../store/auth-context';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -58,6 +60,7 @@ const AuthForm = () => {
     })
     .then((data) => {
       authCtx.login(data.idToken);
+      history.replace('/');
     })
     .catch((err) => {
       alert(err.message)
--- Signup and Authentication/authentication/src/components/Profile/ProfileForm.js
+++ Signup and Authentication/authentication/src/components/Profile/ProfileForm.js
@@ -1,11 +1,44 @@
+import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory();
+
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+    // here if u want to add validation.
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCq0D46LMMQdd83yquIDm-07jK4smD1MD4'
+    ,{
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false, //always use true, but here fal work
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      //we assume this always true so, here i dont put any error handeling
+
+      history.replace('/');
+    })
+
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
