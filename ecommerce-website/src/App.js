95740c64154b1d1cae10101b9a869752f1371d83
MilanTibadiya <tibadiyamilan@gmail.com>
2023-02-12 16:24:35 +0530

    lazy loading/Suspense

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -1,31 +1,38 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Header from "./components/Layout/Header";
-import Products from "./components/Products/Products";
+// import Header from "./components/Layout/Header";
+// import Products from "./components/Products/Products";
+// import Home from "./components/Pages/Home/Home";
+// import About from "./components/Pages/About/About";
+// import ContactUs from './components/Pages/ContactUS/ContactUs';
+// import Product1 from "./components/Products/ProductsImages/Product_1";
+// import Login from "./components/Auth/login";
+// import PrivateRoute from "./components/Pages/PrivateRoute";
+
 import CartProvider from "./store/cartProvider";
 
-import Home from "./components/Pages/Home/Home";
-import About from "./components/Pages/About/About";
-import ContactUs from './components/Pages/ContactUS/ContactUs';
-import Product1 from "./components/Products/ProductsImages/Product_1";
-import Login from "./components/Auth/login";
-
-import PrivateRoute from "./components/Pages/PrivateRoute";
-// let token = localStorage.getItem('token');
+const Header = lazy(() => import("./components/Layout/Header"));
+const Home = lazy(() => import("./components/Pages/Home/Home"));
+const Products = lazy(() => import("./components/Products/Products"));
+const Product1 = lazy(() => import("./components/Products/ProductsImages/Product_1"));
+const About = lazy(() => import("./components/Pages/About/About"));
+const ContactUs = lazy(() => import('./components/Pages/ContactUS/ContactUs'));
+const Login = lazy(() => import("./components/Auth/login"));
+const PrivateRoute = lazy(() => import("./components/Pages/PrivateRoute"));
 
 const router = createBrowserRouter( [ 
   {
     path: '/',
-    element: <Header/>,
+    element: <Suspense><Header/></Suspense>,
     children: [
-      { path: '/', element: <Home/> },
-      { path: '/products', element: <PrivateRoute><Products/></PrivateRoute> }, 
-      { path: '/products/:id', element: <PrivateRoute><Product1/></PrivateRoute> },
-      { path: '/about', element: <About/> },
-      { path: '/contact', element: <ContactUs/>},
-      { path: '/login', element: <Login/>},
-      { path: '*', element:<Login/>}
+      { path: '/', element: <Suspense><Home/></Suspense> },
+      { path: '/products', element: <Suspense><PrivateRoute><Products/></PrivateRoute></Suspense> }, 
+      { path: '/products/:id', element: <Suspense><PrivateRoute><Product1/></PrivateRoute></Suspense> },
+      { path: '/about', element: <Suspense><About/></Suspense> },
+      { path: '/contact', element: <Suspense><ContactUs/></Suspense> },
+      { path: '/login', element: <Suspense><Login/></Suspense> },
+      { path: '*', element:<Suspense><Login/></Suspense> }
     ],
   },
 ] );
